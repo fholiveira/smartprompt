4769a93dabf892de9a966c85d9deab6b789df3a1
Fernando Oliveira <fernando@fholiveira.com>
2014-08-18 23:50:18 -0300

    Add tests to promptsymbol plugin

--- plugins/promptsymbol.go
+++ plugins/promptsymbol.go
@@ -4,10 +4,12 @@ import "syscall"
 
 type PromptSymbol struct{}
 
+var userIsRoot = func() bool { return syscall.Geteuid() == 0 }
+
 func (promptSymbol PromptSymbol) Prompt(parameter string) (string, error) {
 	symbol := "$"
 
-	if syscall.Geteuid() == 0 {
+	if userIsRoot() {
 		symbol = "#"
 	}
 
---
+++ plugins/promptsymbol_test.go
@@ -0,0 +1,25 @@
+package plugins
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPromptSymbolWhenUserIsRoot(t *testing.T) {
+	userIsRoot = func() bool { return true }
+	symbol, err := PromptSymbol{}.Prompt("")
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, "#", symbol)
+	}
+}
+
+func TestPromptSymbolWhenUserIsNotRoot(t *testing.T) {
+	userIsRoot = func() bool { return false }
+	symbol, err := PromptSymbol{}.Prompt("")
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, "$", symbol)
+	}
+}
